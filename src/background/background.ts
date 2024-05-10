72f4934c7f397bd1717d8eb6dc3454f822c4aac3
Eytan3000 <eytankrief@gmail.com>
2024-05-10 17:02:31 +0300

    html cought

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,22 +1,100 @@
-console.log('Background');
+console.log('Background2');
 
-// VARIABLES
-
+// VARIABLES -----------------------------
 // check if user is on a job post
 const job_url_part_str1 = 'linkedin.com/jobs/';
 const job_url_part_str2 = 'currentJobId';
 
+// keystroke names
 const save_job_keystroke = 'save_job_post';
+
+// Global states - Chrome Storage ---------------------
+type GlobalStateKey = 'currentUrl' | 'isJobPost';
+
+function setState(key: GlobalStateKey, value: any) {
+  chrome.storage.sync.set({ [key]: value }, () => {
+    console.log('Data saved');
+  });
+}
+
+function getState(key: GlobalStateKey) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(key, (result) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(result[key]);
+      }
+    });
+  });
+}
+
 // ---------------------
+// helepers:
+function isJobPostFn(url: string) {
+  return url.includes(job_url_part_str1) && url.includes(job_url_part_str2);
+}
 
-let isJobPost: boolean;
-
-// ---------------------
-
-// // TODO: background script
+// ------on Installed -------------------
 // chrome.runtime.onInstalled.addListener(() => {
 //   // TODO: on installed function
 // })
+
+// ------ Listen for tab updates Checks if on job - User clicks and changes job description
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  console.log('changeInfo: ', changeInfo); //removeEytan
+  console.log('changeInfo.status: ', changeInfo.status); //removeEytan
+  if (changeInfo.status === 'complete') {
+    setState('isJobPost', isJobPostFn(tab.url));
+
+    const isJobPost = await getState('isJobPost');
+    if (isJobPost) {
+      console.log('isJob yes');
+
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        // Send a message to the content script in the active tab
+        chrome.tabs.sendMessage(tabs[0].id, { message: 'msg_tabs.onUpdated' });
+      });
+    }
+  }
+});
+
+// ------ Listen for keyboard shortcuts -----------------
+chrome.commands.onCommand.addListener(async (command) => {
+  const isJobPost = await getState('isJobPost');
+  if (command === save_job_keystroke && isJobPost) {
+    console.log('save_job_post');
+
+    // trying to send message to content script to catch html element:
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      // Send a message to the content script in the active tab
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: 'msg_commands.onCommand',
+      });
+    });
+
+    // if job post is saved already >> message "already saved"
+    // if job post is not saved, use the already hashed key and html element and send them to the
+  }
+});
+
+// ---Listen to tab changes, update isJobPost ---------
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  console.log('eta');
+
+  const currentTab = await getCurrentTab();
+  setState('isJobPost', isJobPostFn(currentTab.url));
+});
+
+// -------------------------------------------
 
 // // Listen for messages from content scripts
 // chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
@@ -26,26 +104,20 @@ let isJobPost: boolean;
 //   sendResponse({ reply: "Message received by background script!" });
 // });
 
-// ------Checks if on job - User clicks and changes job description
+// -- wait for content to load (onCompleted) ------
+chrome.webNavigation.onCompleted.addListener(function (details) {
+  // Check if the navigation occurred in the main frame
+  if (details.frameId === 0) {
+    console.log('Page finished loading:', details.url);
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    const currentUrl = tab.url;
-
-    isJobPost =
-      currentUrl.includes(job_url_part_str1) &&
-      currentUrl.includes(job_url_part_str2);
+    chrome.tabs.sendMessage(details.tabId, { action: 'pageLoaded' });
   }
 });
 
-// ------ Listen for keyboard shortcuts -----------------
+// tab updates
+// wait to load
+// sends message to content script
 
-// chrome.commands
-chrome.commands.onCommand.addListener(function (command) {
-  if (command === save_job_keystroke && isJobPost) {
-    console.log('save_job_post');
-
-    // if job post is saved already >> message "already saved"
-    // if job post is not saved, use the already hashed key and html element and send them to the
-  }
-});
+chrome.webNavigation.onDOMContentLoaded.addListener(() =>
+  console.log('content loaded!')
+);
--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,17 +1,55 @@
-console.log('Content Script Active');
+// * copy the classnames as is
 
+const JobPostClass = 'jobs-search__job-details--wrapper';
+const jobTitleClass = '.job-details-jobs-unified-top-card__job-title';
 // --------------------------------------------------------------------------
-/**
+
 // job post html element catch: ----------------------------------------
 
-const jobTitleHtmlElement = document.querySelector(
-  '.job-details-jobs-unified-top-card__job-title'
-);
-const entireJobPostHtmlEl = document.querySelector(
-  '.jobs-details__main-content'
-);
+const jobTitleHtmlElement = document.querySelector(jobTitleClass);
+const entireJobPostHtmlEl = document.querySelector(JobPostClass);
 
-console.log(entireJobPostHtmlEl.outerHTML);
+const divsToRemove = [
+  'jobs-premium-applicant-insights__applicant-location-map',
+  'jobs-premium-company-insights artdeco-card premium-accent-bar',
+  'jobs-company jobs-box--fadein mb4 jobs-company--two-pane',
+  'coach-shared-hscroll-bar__multi-container',
+  'jobs-s-apply jobs-s-apply--fadein inline-flex mr2',
+  'jobs-save-button artdeco-button artdeco-button--3 artdeco-button--secondary',
+  'jobs-premium-applicant-insights__reset-map-button p2',
+];
+
+// -- Helpers --------------------
+function wait(sec: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(null);
+    }, sec * 1000);
+  });
+}
+function formatClassNameForQuerySelector(className: string) {
+  return '.' + className.replaceAll(' ', '.'); // 'main-div flex'>>'.main-div.flex'
+}
+async function retryUntilNotNull(divClass: string) {
+  // divClass is what you copy from the classname of the div, with spaces
+  const divToGet = formatClassNameForQuerySelector(divClass);
+  console.log('divToGet: ', divToGet); //removeEytan
+  return new Promise(async (resolve, reject) => {
+    let divToRemove = null;
+    let count = 0;
+    while (divToRemove === null) {
+      count++;
+      if (count > 10) reject(`className: ${divClass} not found!`);
+      await wait(0.5);
+      divToRemove = document.querySelector(divToGet);
+    }
+    console.log(`Waited 0.5 sec ${count} times.`);
+    resolve(divToRemove);
+  });
+}
+
+// entireJobPostHtmlEl.removeChild()
+// console.log(entireJobPostHtmlEl.outerHTML);
 
 // let actualJobTitle: string;
 // if (entireJobPostHtmlEl) {
@@ -21,11 +59,72 @@ console.log(entireJobPostHtmlEl.outerHTML);
 //   console.log('Element not found.');
 // }
 
-// Send a message from content script to background script
-chrome.runtime.sendMessage(
-  { message: entireJobPostHtmlEl.outerHTML },
-  function (response) {
-    console.log('Response from background script:', response);
+// // Send a message from content script to background script
+// chrome.runtime.sendMessage(
+//   { message: entireJobPostHtmlEl.outerHTML },
+//   function (response) {
+//     console.log('Response from background script:', response);
+//   }
+// );
+
+// -- Listen to background ---
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   if (message.message === 'myMessage') {
+//     console.log('received message');
+//     const x = document.querySelector(
+//       '.jobs-premium-company-insights.artdeco-card.premium-accent-bar'
+//     );
+//     console.log('x: ', x); //removeEytan
+//   }
+// });
+
+// -----------------------------
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  // Check if the message action is "pageLoaded"
+  if (message.action === 'pageLoaded') {
+    // Perform actions here after the page has finished loading
+    console.log('Content script received message: Page finished loading.');
+
+    // const x = document.querySelector(
+    //   '.jobs-premium-company-insights.artdeco-card.premium-accent-bar'
+    // );
+    // console.log('pageLoadedX: ', x); //removeEytan
   }
-);
- */
+
+  // User clicks on a job on left column:
+  if (message.message === 'msg_tabs.onUpdated') {
+    try {
+      const jobPostEl = (await retryUntilNotNull(
+        JobPostClass
+      )) as HTMLElement | null;
+      console.log(jobPostEl);
+
+      const clonedJobPostEl = jobPostEl.cloneNode(true) as HTMLElement | null; // if you dont clone the element, all changes will affect the actual DOM.
+
+      divsToRemove.forEach((className) => {
+        const elementToRemove = clonedJobPostEl.querySelector(
+          formatClassNameForQuerySelector(className)
+        );
+        if (elementToRemove) {
+          elementToRemove.remove();
+        }
+      });
+
+      console.log('124: ', clonedJobPostEl);
+
+      // ------------------------------------------------------
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  if (message.message === 'msg_commands.onCommand') {
+    console.log('received message: msg_commands.onCommand');
+
+    const divToRemove2 = document.querySelector(
+      '.jobs-premium-company-insights.artdeco-card.premium-accent-bar'
+    );
+
+    console.log('onCommand - divToRemove2: ', divToRemove2); //removeEytan
+  }
+});
